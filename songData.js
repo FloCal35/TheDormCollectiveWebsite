5f43a2c37f9c8694f2bf17c3545b781b9e848232
FloCal35 <flocal305@gmail.com>
2025-05-21 17:01:05 -0500

    updated info

--- songData.js
+++ songData.js
@@ -4,10 +4,10 @@ const songs = {
         blurryImg:"imgs/ImperfectBlur.png",
         title: "Imperfect",
         releaseDate: "January 24, 2025",
-        genre: "Pop Ballad",
+        genre: "Ballad",
         about:"Imperfect was the first song by The Dorm Collective, written by Caleb and Aidan during their first semester at Belmont. Recorded and released at the beginning of their second semester, it kickstarted The Dorm Collective’s journey. This heartfelt song explores how flaws can create deep, meaningful relationships. It opens with one person dreaming of an ideal version of themselves and the perfect relationship. In responce, another voice comes and reassures them that these imperfections are what make them human and worthy of love. The final moments of the song emphasize unity and the authenticity of relationships. It ends with shared realization that no one can be everything on their own - we all need someone by our side.",
         lyrics:"V1 <br>If I was perfect <br>I’d be everything you need my love<br>I don’t know how<br>We’d be together<br>I wouldn’t miss you<br>I’d be holding you right now",
-        credits:[["Aiden", "Aiden's page","Melody, Acoustic Guitar"], 
+        credits:[["Aidan McCarel", "Aiden's page","Melody, Acoustic Guitar"], 
             ["Caleb Flores", "Caleb's page link", "Lyrics, Vocals, Production"],
             ["Ella Markwald","Ella's page link","Vocals, Assist in lyrics"],
             ["Clark Nelson","Clark's page link","Assist in production"]],
@@ -16,25 +16,29 @@ const songs = {
     "darlingGirl":{
         //calebbbbb please filll this in
         image: "imgs/DarlingGirl.jpeg",
-        blurryImg:"imgs/DarlingGirl.jpeg",
+        blurryImg:"imgs/DGBlur.png",
         title: "Darling Girl",
-        releaseDate: "January 24, 2025",
-        genre: "Pop Ballad",
-        about:"ella likes girls",
+        releaseDate: "March 28, 2025 (Under Ella Markwald)",
+        genre: "Pop",
+        about:".........",
         lyrics:"idk it says darling girl at som point",
-        credits:[["Ella Markwald", "Ella's page link", "Vocals"], ["Caleb Flores", "Caleb's page","production?"]],
+        credits:[["Ella Markwald", "Ella's page link", "Melody, Lyrics, Vocals"],
+            ["Aidan McCarel", "Caleb's page","Guitar"],
+            ["Austin Windau", "Caleb's page","Bass Guitar"],
+            ["Caleb Flores", "Caleb's page","Production"],
+            ["Clark Nelson", "Caleb's page","Piano, Assist in production"],],
         links:["https://music.apple.com/us/song/darling-girl/1802281141","https://open.spotify.com/track/2mqf0xVx8FMzzSKG6wgofP?si=204f4e87941a4a84","https://music.youtube.com/watch?v=8CxzF_N55uY&si=hQbT_YKq-siDjpto"]
     },
     "manInSpace":{
         //calebbbbb please filll this in
         image: "imgs/ManInSpaceFinal.png",
-        blurryImg:"imgs/ManInSpaceFinal.png",
-        title: "Man In Space",
+        blurryImg:"imgs/MISBlur.png",
+        title: "Man In Space (Close To You)",
         releaseDate: "February 14, 2025",
-        genre: "techy instrumental",
-        about:"Cooper likes music and space",
+        genre: "Jungle/Drum and Bass",
+        about:".........",
         lyrics:"N/A",
-        credits:[["Cooper", "Cooper's page","idk"]],
+        credits:[["Cooper", "Cooper's page","Melody, Production"]],
         links:["https://music.apple.com/us/song/man-in-space-close-to-you/1795380990","https://open.spotify.com/track/5soxVCjsuNG7t0iMxI0sXh?si=54bbe34fc54f4d56","https://music.youtube.com/watch?v=6si9U9UFmZc&si=8jE3HBwG4N7kRdQQ"]
     }
 }
