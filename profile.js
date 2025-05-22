5d0d09e20ae385efc7193912f189c77e322a29e4
AnnaChason <annachason.wiggins@my.wheaton.edu>
2025-05-21 22:08:34 -0500

    started setting up javascript for profile page

---
+++ profile.js
@@ -0,0 +1,23 @@
+const people={
+    "caleb":{
+        Name: "Caleb Flores",   
+        Instrument: "Voice, Producer/DAW",
+        Major: "Music Production",
+        About: "Caleb Flores is one of the many music majors at Belmont University. His musical journey began at the age of 4 where he began his 14 year violin journey. A few years later, he began his 10 year career in the Birmingham Boys Choir. In high school, he travelled the country (and globe) as a saxaphone section leader for the Homewood Patriot Marching Band and a dancer and historian for The Network show choir. Performance isn’t the only aspect of music he enjoys. He was trained by a previous Belmont graduate in Audio Engineering Technology and has been able to work for clients in multiple venues. The Dorm Collective is special to him because collaboration is what brings out his creative spark.",
+        Email: "flocal35@outlook.com"
+    },
+    "aidan":{
+        Name: "Aidan McCarel",
+        Instrument: "Guitar",
+        Major: "Commercial Guitar",
+        About:"",
+        Email:""
+    }
+}
+
+/*
+getting elements
+*/
+const nameEl = document.getElementById("name");
+const roleEl = document.getElementById("role");
+const detailEls = document.querySelectorAll("div.left span");
