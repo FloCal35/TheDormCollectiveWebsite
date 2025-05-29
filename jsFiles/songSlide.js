09b61d724cbb02b64aa5a100bccd8bb96bead095
AnnaChason <annachason.wiggins@my.wheaton.edu>
2025-05-29 15:51:07 -0500

    minor styling fixes

--- jsFiles/songSlide.js
+++ jsFiles/songSlide.js
@@ -21,10 +21,24 @@ for(let i=0; i<person.Songs.length; i++){
             .then(html => {
             document.getElementById('songSlideHolder').innerHTML += html;
             
-            slides = getelementsByClass(songContainer)
-            for(songName of person.Songs){
-                song = songs[songName];
-                
+            /*
+            load data
+            */
+            const slides = document.getElementsByClassName('songContainer');
+            for(let i=0; i<slides.length; i++){
+                var song = songs[person.Songs[i]];
+                var slide = slides[i];
+
+                var coverEl = slide.querySelector("img");
+                var titEl = slide.querySelector('h1');
+                var linkEls = slide.querySelectorAll("a");
+                slide.style.backgroundImage = `url('${song.blurryImg}')`;
+
+                coverEl.src = song.image;
+                titEl.innerHTML = song.title;
+                for(let i=0; i < linkEls.length; i++){
+                    linkEls[i].setAttribute("href", song.links[i]);
+                }
             }
         });
     }
