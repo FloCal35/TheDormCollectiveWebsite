b358352f72a46b04bc0164cb31fb5d653ef19122
FloCal35 <flocal305@gmail.com>
2025-06-01 16:42:41 -0500

    Release 1.1

--- jsFiles/profile.js
+++ jsFiles/profile.js
@@ -6,7 +6,7 @@ const people={
         Major: "Music Production",
         About: "Caleb Flores is a music major at Belmont University. His musical journey began at the age of 4 where he began his 14 year violin journey. A few years later, he began his 10 year career in the Birmingham Boys Choir. In high school, he travelled the country (and globe) as a saxaphone section leader for the Homewood Patriot Marching Band and a singer/dancer and historian for The Network show choir. Performance isn’t the only aspect of music he enjoys. He was trained by a previous Belmont graduate in Audio Engineering Technology and has been able to work for clients in multiple venues. The Dorm Collective is special to him because collaboration is what brings out his creative spark.",
         Email: "flocal35@outlook.com",
-        Songs: ["imperfect","darlingGirl","manInSpace"]
+        Songs: ["imperfect","darlingGirl"]
     },
     "aidan":{
         Img: "imgs/AidanProfile.png",
