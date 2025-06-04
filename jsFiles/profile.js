8afd1440b66392227adcf441c8b1fb35fe26a3eb
FloCal35 <flocal305@gmail.com>
2025-06-04 16:35:11 -0500

    Release 1.2

--- jsFiles/profile.js
+++ jsFiles/profile.js
@@ -6,7 +6,7 @@ const people={
         Major: "Music Production",
         About: "Caleb Flores is a music major at Belmont University. His musical journey began at the age of 4 where he began his 14 year violin journey. A few years later, he began his 10 year career in the Birmingham Boys Choir. In high school, he travelled the country (and globe) as a saxaphone section leader for the Homewood Patriot Marching Band and a singer/dancer and historian for The Network show choir. Performance isn’t the only aspect of music he enjoys. He was trained by a previous Belmont graduate in Audio Engineering Technology and has been able to work for clients in multiple venues. The Dorm Collective is special to him because collaboration is what brings out his creative spark.",
         Email: "flocal35@outlook.com",
-        Songs: ["imperfect","darlingGirl"]
+        Songs: ["imperfect","darlingGirl","odeToLife","whenShesThere","movement"]
     },
     "aidan":{
         Img: "imgs/AidanProfile.png",
@@ -15,16 +15,16 @@ const people={
         Major: "Commercial Guitar",
         About:"Aidan McCarel, a guitarist at Belmont University, plays all different varieties of styles and genres but succeeds heavily in making music that makes people turn the radio off. Although he lives in Philedelphia, he is a native of Ohio and a die hard Cleveland Browns fan (He “Will absolutely tattoo his body once they win a championship”). Aidan has been writing music for the better part of 4 years and has always had a passion working with and being around people. He shares a bathroom with the members of the Dorm Collective during the Fall and Spring semester",
         Email:"",
-        Songs: ["imperfect","darlingGirl"]
+        Songs: ["imperfect","darlingGirl","odeToLife","muddyWaters","whenShesThere","movement"]
     },
     "cooper":{
-        Img: "imgs/CooperProfile.jpg",
+        Img: "imgs/CooperProfile.png",
         Name: "Cooper Chu",
         Instrument: "Producer/DAW",
         Major: "Audio Engineering Technology",
-        About:"I am a guy",
-        Email:"dont contact me",
-        Songs: ["manInSpace"]
+        About:"",
+        Email:"",
+        Songs: ["manInSpace","muddyWaters"]
     },
     "austin":{
         Img: "imgs/AustinProfile.png",
@@ -33,7 +33,7 @@ const people={
         Major: "Commercial Bass Guitar",
         About: "Austin Windau is a bassist at Belmont University. His musical path started in 6th grade in his hometown of Westlake, Ohio. He’s been into music since a young age and started out as a school of rock kid. Throughout high school, he spent most of his time practicing and teaching himself everything about music. In college, he’s a part of several groups: The Dorm Collective, Scuffed Animals, and The Lizzies.  He is currently in college figuring out where in the music world he fits best.",
         Email:"",
-        Songs: ["darlingGirl"]
+        Songs: ["darlingGirl","movement"]
     },
     "dean":{
         Img:"imgs/deanProfile.JPG",
@@ -51,16 +51,16 @@ const people={
         Major: "Commercial Voice",
         About: "Ari Andelman is a songwriter at Belmont University from Washington, D.C. His notable accolades include placing 2nd in the Vocal Arts Society Competition, performing with the Temple Choir in Nashville, singing a solo in Sounds of Belmont at the Fisher Center, and performing as a lead role in the opera, The Gondoliers, at the Duke Ellington School of the Arts. So far, he has released his first single, “Step Out the Frame”, and is working at the Musicians Corner festival as an Audio Engineering intern for the summer. The Dorm Collective is something that he is proud to have collaborated with, and he hopes to work on more projects with these talented musicians in the upcoming years.",
         Email:"",
-        Songs: []
+        Songs: ["odeToLife"]
     },
     "ella":{
         Img:"imgs/EllaProfile.png",
         Name: "Ella Markwald",
         Instrument:"Voice",
-        Major: "...",
+        Major: "Music Composition",
         About: "Ella Markwald is a musician originally from Milwaukee, Wisconsin. She currently resides in Nashville, Tennessee, while studying Music Composition, Music Theory, and Music Business at Belmont University. She’s been singing and playing the violin since age 6, and has been writing songs since middle school. ",
         Email:"",
-        Songs: ["imperfect","darlingGirl"]
+        Songs: ["imperfect","darlingGirl","muddyWaters","movement"]
     },
     "carter":{
         Img:"imgs/CarterProfile.png",
@@ -69,7 +69,16 @@ const people={
         Major: "Commercial Guitar",
         About: "Carter’s music journey began at the age of 4, when he received his first guitar as a Christmas present. Living in Atlanta, Georgia, he began to study under award-winning artist Eliot Bronson who at the time was an up and coming songwriter. Carter’s mom began to take him to concerts of country stars which serve as inspiration for his musical identity. Through the years, Carter has stumbled upon numerous other sources of inspiration and his list of influences now grows larger by the day. While Carter’s sound currently reflects a pop singer-songwriter style, almost all of his influences find their way into his music in one way or another. Carter has performed at distinguished venues like Eddie's Attic and Cadence Bank Amphitheatre, shared the stage with acclaimed musicians including John Driscoll Hopkins (of the Zac Brown Band), and Brian Brasher (of Creed), and raised over $50,000 for Children's Healthcare of Atlanta Hospital through yearly solo concerts. Additionally, he has recently released his debut single recorded with two-time Grammy winning producer Don McCollister.",
         Email:"",
-        Songs: [""]
+        Songs: ["whenShesThere"]
+    },
+    "clark":{
+        Img:"imgs/ClarkProfile.png",
+        Name: "Clark Nelson (MythSix)",
+        Instrument:"Piano, Producer/DAW",
+        Major: "Commercial Piano",
+        About: "",
+        Email:"",
+        Songs: ["imperfect","darlingGirl","odeToLife","whenShesThere","movement"]
     }
 }
 
--- jsFiles/script.js
+++ jsFiles/script.js
@@ -1,1 +1,11 @@
-//moved to navscript.js
+const openBtn = document.getElementById("openPopup");
+const closeBtn = document.getElementById("closePopup");
+const popup = document.getElementById("popup");
+
+openBtn.addEventListener("click", () => {
+    popup.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    popup.classList.remove("active");
+});
