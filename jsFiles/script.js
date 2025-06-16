949bb6630c0a7284ace335825c7f68b7c7fcd50f
AnnaChason <annachason.wiggins@my.wheaton.edu>
2025-06-16 17:27:45 -0500

    furthered homescreen mobile compatability

--- jsFiles/script.js
+++ jsFiles/script.js
@@ -4,8 +4,10 @@ const popup = document.getElementById("popup");
 
 openBtn.addEventListener("click", () => {
     popup.classList.add("active");
+    openBtn.classList.add("active");
 });
 
 closeBtn.addEventListener("click", () => {
     popup.classList.remove("active");
+    openBtn.classList.remove("active");
 });
