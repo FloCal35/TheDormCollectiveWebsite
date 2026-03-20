3d093c6fd7c49f8001f9228de5248cc0abf6527b
FloCal35 <flocal305@gmail.com>
2026-03-20 11:36:02 -0500

    profiles

--- jsFiles/profile.js
+++ jsFiles/profile.js
@@ -6,7 +6,7 @@ const people={
         Major: "Music Production",
         About: "Caleb Flores is a music major at Belmont University. His musical journey started at the age of 4 where he began his 14 year violin journey. A few years later, he began his 10 year career in the Birmingham Boys Choir. In high school, he travelled the country (and globe) as a saxaphone section leader for the Homewood Patriot Marching Band and a singer/dancer for The Network show choir. Performance isn’t the only aspect of music he enjoys. He was trained by a previous Belmont graduate in Audio Engineering Technology and has been able to work for clients in multiple venues like The Fisher Center. The Dorm Collective is special to him because collaboration is what brings out his creative spark.",
         Email: "flocal35@outlook.com",
-        Songs: ["endOfTheRoad","imperfect","stillTheOne","darlingGirl","odeToLife","whenShesThere","movement"]
+        Songs: ["endOfTheRoad","imperfect","stillTheOne","darlingGirl","odeToLife","whenShesThere","movement","heavensLine"]
     },
     "aidan":{
         Img: "imgs/AidanProfile.png",
@@ -15,7 +15,7 @@ const people={
         Major: "Commercial Guitar",
         About:"Aidan McCarel, a guitarist at Belmont University, plays all different varieties of styles and genres but succeeds heavily in making music that makes people turn the radio off. Although he lives in Philedelphia, he is a native of Ohio and a die hard Cleveland Browns fan (He “Will absolutely tattoo his body once they win a championship”). Aidan has been writing music for the better part of 4 years and has always had a passion working with and being around people. He shares a bathroom with the members of the Dorm Collective during the Fall and Spring semester",
         Email:"",
-        Songs: ["endOfTheRoad","dreamin","imperfect","odeToLife","muddyWaters","whenShesThere","movement","stillTheOne","darlingGirl"]
+        Songs: ["heavensLine","endOfTheRoad","dreamin","imperfect","odeToLife","muddyWaters","whenShesThere","movement","stillTheOne","darlingGirl"]
     },
     "cooper":{
         Img: "imgs/CooperProfile.png",
@@ -42,7 +42,7 @@ const people={
         Major: "Commercial Music",
         About: "Dean Marino is an exciting upcoming act from Belmont University. His catalog consists of 7 albums and hundreds of songs. A native of Sugar Land, Texas, Dean would describe his music as somewhere between Billy Joel and Carly Rae Jepsen, though he collects new influences often. Dean is thrilled to be working so closely with The Dorm Collective on various projects and thinks that collaboration is what makes music incredible.",
         Email:"",
-        Songs: ["endOfTheRoad"]
+        Songs: ["endOfTheRoad","heavensLine"]
     },
     "ari":{
         Img:"imgs/AriProfile.png",
@@ -69,7 +69,7 @@ const people={
         Major: "Commercial Guitar",
         About: "Carter’s music journey began at the age of 4, when he received his first guitar as a Christmas present. Living in Atlanta, Georgia, he began to study under award-winning artist Eliot Bronson who at the time was an up and coming songwriter. Carter’s mom began to take him to concerts of country stars which serve as inspiration for his musical identity. Through the years, Carter has stumbled upon numerous other sources of inspiration and his list of influences now grows larger by the day. While Carter’s sound currently reflects a pop singer-songwriter style, almost all of his influences find their way into his music in one way or another. Carter has performed at distinguished venues like Eddie's Attic and Cadence Bank Amphitheatre, shared the stage with acclaimed musicians including John Driscoll Hopkins (of the Zac Brown Band), and Brian Brasher (of Creed), and raised over $50,000 for Children's Healthcare of Atlanta Hospital through yearly solo concerts. Additionally, he has recently released his debut single recorded with two-time Grammy winning producer Don McCollister.",
         Email:"",
-        Songs: ["endOfTheRoad","whenShesThere"]
+        Songs: ["heavensLine","endOfTheRoad","whenShesThere"]
     },
     "clark":{
         Img:"imgs/Clark.png",
@@ -87,10 +87,10 @@ const people={
         Major: "Commercial Voice",
         About: "",
         Email:"",
-        Songs: ["stillTheOne"]
+        Songs: ["stillTheOne","heavensLine"]
     },
     "jolie":{
-        Img:"imgs/TDCTransB.png",
+        Img:"imgs/Jolie.png",
         Name: "Jolie Claire",
         Instrument:"Voice",
         Major: "Commercial Voice",
